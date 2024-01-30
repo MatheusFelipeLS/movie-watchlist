291d6de5777a95815078ffc699670b32f0643e0e
Matheus Felipe <mathfelpessoal@gmail.com>
2024-01-30 09:59:03 -0300

    Adição e personalização do formulário de novos filmes

--- movie_library/routes.py
+++ movie_library/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, request
+from movie_library.forms import MovieForm
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
@@ -9,6 +10,20 @@ def index():
     return render_template(
         "index.html",
         title="Movies watchlist"
+    )
+    
+    
+@pages.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = MovieForm()
+    
+    if request.method == "POST":
+        pass
+    
+    return render_template(
+        "new_movie.html", 
+        title="Movie Watchlist - Add Movie", 
+        form=form
     )
     
 
